d7e67ff186502f575f8a3a7620ebd0b8e7f40639
Tyler <tahbristol@gmail.com>
2018-07-13 23:02:48 -0400

    working index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
 
 	register Sinatra::Reloader
 
-	configure do 
+	configure do
 		enable :sessions
 		set :session_secret, "my_application_secret"
 		set :public_folder, Proc.new{File.join(root,"../../public")}
@@ -16,4 +16,5 @@ class ApplicationController < Sinatra::Base
 	get '/' do
 		erb :index
 	end
-
+	
+end
